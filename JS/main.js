1dcb0b5523a7d3d0348f415e046ccaa833ad6e7e
matteodilorenzo21 <matteodilorenzo21@gmail.com>
2023-06-20 20:22:17 +0200

    added Vue js Script milestones

---
+++ JS/main.js
@@ -0,0 +1,42 @@
+const app = Vue.createApp({
+    data() {
+      return {
+        newTask: '',
+        tasks: [
+          {
+            text: 'fare la spesa',
+            done: false,
+          },
+          {
+            text: 'passeggiare il cane',
+            done: false,
+          },
+          {
+            text: 'lavare la macchina',
+            done: false,
+          },
+          {
+            text: 'firmare il contratto',
+            done: false,
+          },
+        ],
+      };
+    },
+    methods: {
+      // Funzione per eliminare il singolo task
+      removeTask(targetIndex) {
+        this.tasks.splice(targetIndex, 1);
+      },
+  
+      // Funzione per aggiungere una task
+      addTask() {
+        this.tasks.push({
+          text: this.newTask,
+          done: false,
+        });
+        this.newTask = ''; // Resetta il campo di input dopo l'aggiunta della task
+      },
+    },
+  });
+  
+  app.mount('#root');
